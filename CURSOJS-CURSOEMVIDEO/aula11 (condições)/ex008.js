a064a7a899330ef8c535e01cc52e140d61804dcd
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2023-11-02 22:42:30 -0300

    Alterando exercícios
    Fazendo modificações na aula 11 de JavaScript (Condições)

--- CURSOJS-CURSOEMVIDEO/aula11 (condições)/ex008.js
+++ CURSOJS-CURSOEMVIDEO/aula11 (condições)/ex008.js
@@ -1,6 +1,6 @@
 var vel = 70
 console.log(`A velocidade do seu carro é ${vel}km/h`)
-if (vel > 60) { //Condição simples!
+if (vel > 60) { //Condição simples! > Não tem 'else' (Se não...)
     console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
 }
 console.log(`Dirija sempre usando cinto de segurança!`)
