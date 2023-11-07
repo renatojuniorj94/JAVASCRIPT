d4c8a5b5f82a9a797b40629afff11e19abbf3fa0
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2023-11-07 00:51:40 -0300

    Exercício aula 12 - parte 2

--- CURSOJS-CURSOEMVIDEO/aula12 (exercicios)/ex014/script.js
+++ CURSOJS-CURSOEMVIDEO/aula12 (exercicios)/ex014/script.js
@@ -1,1 +1,21 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 18
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'fotos/dia.png'
+        document.body.style.background = '#73a7f7'
+    } else if (hora >= 12 && hora <18) {
+        //BOA TARDE!
+        img.src = 'fotos/tarde.png'
+        document.body.style.background = '#dcb883'
+    } else {
+        //BOA NOITE!
+        img.src = 'fotos/noite.png'
+        document.body.style.background = '#022e5a'
+    }
+}
