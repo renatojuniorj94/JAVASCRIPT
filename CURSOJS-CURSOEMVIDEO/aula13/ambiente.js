27a8f4f42dc003770bb0bdd2cbbb9c04e2855ac2
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2023-11-09 16:25:52 -0300

    Aula 13 - Estrutura de repetição
    Parte 1

---
+++ CURSOJS-CURSOEMVIDEO/aula13/ambiente.js
@@ -0,0 +1,12 @@
+/* Estrutura de repetição com teste lógico no início
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} */
+
+var c = 1 //Estrura de repetição com teste lógico no final
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10) 
