b489505b49e1ff5f1c89467ce1e2c993e55c1ebf
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2023-11-10 20:33:23 -0300

    Aula 14 - estrutura de repetição (parte 2)
    Aprendendo "for" > Estrutura de repetição com variável de controle. Aprendemos também a usar console de depuração no Vs Code.

---
+++ CURSOJS-CURSOEMVIDEO/aula14 (Repetições)/ambiente.js
@@ -0,0 +1,28 @@
+/*
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++ // c += 1 ou c = c + 1
+}
+
+Ou
+
+var c = 1
+do {
+    console.log(c)
+    c++
+} while (c <= 5)
+
+Ou
+
+
+for (var c = 1; c <= 5; c++) {
+    console.log(c)
+}
+*/
+
+console.log('Vai começar...')
+for(var c = 1; c <= 4; c++) {
+    console.log(c)
+}
+console.log('FIM!')
