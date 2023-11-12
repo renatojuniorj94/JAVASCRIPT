53e46d15ecc4eee6601f4408f4ec3601e7fad73c
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2023-11-12 18:37:54 -0300

    Aula 16 - Funções
    Link da aula :
    https://www.youtube.com/watch?v=mc3TKp2XzhI&list=PLHz_AreHm4dlsK3Nr9GVvXCbpQyHQl1o1&index=30

---
+++ CURSOJS-CURSOEMVIDEO/aula16 (Funções)/funcao01.js
@@ -0,0 +1,16 @@
+function parimpar(n) {
+    if (n % 2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+
+/* 
+let res = parimpar(4)
+console.log(res) 
+
+Ou
+*/
+
+console.log(parimpar(4))
