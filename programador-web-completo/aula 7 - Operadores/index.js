13f045bc32abddf367b84abc3ada7a05345bac9a
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2023-11-23 14:38:52 -0300

    Aula 7 - Operadores

---
+++ programador-web-completo/aula 7 - Operadores/index.js
@@ -0,0 +1,10 @@
+var valor1, valor2, total, total2
+valor1 = 5
+valor2 = 2
+
+total = valor1 + valor2
+
+total2 = ++valor1 //++ é um incremento, no caso ele adiciona mais 1 número.
+// -- Tira um número.
+
+console.log(`O total é ${total}`)
