2d864fb97e8c299a730d4691aa92da58901674c2
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2023-11-29 12:43:34 -0300

    Conclusão aula 10 - Eventos

--- programador-web-completo/aula 10 - Eventos/index.js
+++ programador-web-completo/aula 10 - Eventos/index.js
@@ -22,3 +22,24 @@ function adicionaTexto() {
     //append > adicionar
     p.append("O mouse moveu")
 }
+
+let focoRecebido = false
+let backgroundColorAnterior
+
+function campoFocado() {
+    if (!focoRecebido) {
+         // Armazena o backgroundColor original antes de mudá-lo
+         backgroundColorAnterior = document.body.style.backgroundColor
+        // Faça a ação desejada, por exemplo, mudar a cor de fundo
+        document.body.style.backgroundColor = 'lightgray'
+        //alert('Campo focado!') > Em onfocus ao usar o 'alert' aconte um bug, em que a mensagem não desaparece ao clicar em 'ok'
+        focoRecebido = true // Atualiza a variável para indicar que o foco foi recebido
+    }
+}
+
+function fimBug() {
+    // Restaura o backgroundColor original ao perder o foco
+    document.body.style.backgroundColor = backgroundColorAnterior
+    // Reseta a variável quando o campo perde o foco
+    focoRecebido = false;
+}
