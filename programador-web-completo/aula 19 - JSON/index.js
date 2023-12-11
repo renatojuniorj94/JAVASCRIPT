f2eaac3ca5966baac95501fa57ce319f0427c76f
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2023-12-11 01:48:26 -0300

    Aula 19 - JSON (Parte 2, conclusão)

--- programador-web-completo/aula 19 - JSON/index.js
+++ programador-web-completo/aula 19 - JSON/index.js
@@ -21,3 +21,37 @@ let obj = JSON.parse(texto)
 //Pegamos um valor desse objeto
 console.log(obj.modelo)
 console.log(obj.motor[2])
+
+const ajax = new XMLHttpRequest()
+//Pegando(GET) informações de um site, no caso '01001000' é um cep
+ajax.open('GET', 'https://viacep.com.br/ws/01001000/json/')
+//Enviando a solicitação
+ajax.send()
+
+ajax.onload = function () {
+    //Resposta em texto vindo da requisição acima
+    document.getElementById('area').innerHTML = this.responseText
+    //Para pegar um determinado item do site acima, primeiro temos que converter o arquivo em objeto
+    let obj = JSON.parse(this.responseText)
+    /*alert(obj.ddd)*/
+}
+
+function buscarCEP() {
+    let input = document.getElementById('cep').value
+
+    const ajax2 = new XMLHttpRequest()
+    ajax2.open('GET', 'https://viacep.com.br/ws/'+ input +'/json/')
+    ajax2.send()
+
+    ajax2.onload = function () {
+        document.getElementById('texto').innerHTML = this.responseText
+        //Pegando somente algumas informações do site
+        let obj = JSON.parse(this.responseText)//Aqui transformamos em objeto
+        let logradouro = obj.logradouro
+        let cidade = obj.localidade
+        let estado = obj.uf
+
+        document.getElementById('texto').innerHTML = "Logradouro: " + logradouro + "<br> Cidade: " + cidade + "<br> Estado: " + estado
+    }
+}
+
