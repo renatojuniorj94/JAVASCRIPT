2099ef46e62af2968e0ad71440c6a439a47ad016
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-10-13 01:24:25 -0300

    Desafio 2 (Custos da viajem v2.0)

---
+++ formaçãoJavaScriptDio/desafios/desafio2.js
@@ -0,0 +1,29 @@
+const etanol = 4.79
+const gasolina = 5.69
+const combustivelAtual = gasolina
+const mediaConsumoKm = 15
+const kmPercorrido = 472
+
+const resultado = (kmPercorrido / mediaConsumoKm) * combustivelAtual
+let mensagem = 'O total gasto na viajem foi de R$'.replace("s", "$")
+console.log(`${mensagem} ${resultado.toFixed(2).replace(".", ",")}`)
+
+//Correto! :D
+//Outra maneira de fazer
+
+const alcool = 4.79
+const gasolinaComum = 5.69
+const combustivelUsado = "gasolina"
+const consumoPorKm = 15
+const kmTotal = 472
+
+//OBS: Usamos 'var' por ser de escopo global, ou definimos o console.log dentro da estrutura condicional
+if (combustivelUsado === "gasolina") {
+    var res = (kmTotal / consumoPorKm) * gasolinaComum
+    /* console.log(`O total gasto na viagem foi ${res.toFixed(2).replace(".", ",")}`) */
+} else {
+    var res = (kmTotal / consumoPorKm) * alcool
+    /* console.log(`O total gasto na viagem foi ${res.toFixed(2).replace(".", ",")}`) */
+}
+
+console.log(`O total gasto na viagem foi R$ ${res.toFixed(2).replace(".", ",")}`)
